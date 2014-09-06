f106f63e4c9046573d6ec20a51bdd22d3f0c2464
easytargetmixel <michel@easy-target.org>
2014-09-06 21:59:57 +0200

    VLC update path tweak

--- org/eztarget/vidcheroo/VidcherooConfig.java
+++ org/eztarget/vidcheroo/VidcherooConfig.java
@@ -38,8 +38,7 @@ public class VidcherooConfig {
 	// TODO: Find further variations of this.
 	private static final String VLC_DEFAULT_PATH_LIN = "/usr/lib/vlc";
 	private static final String VLC_DEFAULT_PATH_OSX = "/Applications/VLC.app/Contents/MacOS/lib";
-	private static final String VLC_DEFAULT_PATH_WIN = "C:\\Program Files\\VLC\\";
-	private static final String VLC_DEFAULT_PATH_WI2 = "C:\\Program Files (x86)\\VideoLAN\\VLC\\";
+	private static final String VLC_DEFAULT_PATH_WIN = "C:\\Program Files\\VideoLAN\\VLC\\";
 	
 	private static String mediaPath;
 	private static String vlcPath;
